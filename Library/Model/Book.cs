6ac73e1405d4b747ccdc69f466ca7cccb6e24d79
vforsyuk001 <vforsyuk001@gmail.com>
2020-02-11 17:09:49 +0200

    Model base classes

---
+++ Library/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Library.Model
+{
+    public class Book
+    {
+
+
+
+        public Book()
+        {
+
+        }
+    }
+}
---
+++ Library/Model/Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Library.Model
+{
+    public class Client
+    {
+        public Client()
+        {
+        }
+    }
+}
