f9888c70e70c8ff4aeb4944d8d5a4333f16e4641
vforsyuk001 <vforsyuk001@gmail.com>
2020-02-12 13:49:14 +0200

    View Folder

--- Library/View/BaseWindow.xaml.cs
+++ Library/View/BaseWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,21 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Library
+namespace Library.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для BaseWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class BaseWindow : Window
     {
-        public MainWindow()
+        public BaseWindow()
         {
             InitializeComponent();
-
-            
         }
     }
 }
